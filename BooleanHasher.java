751737dceef75944b0794b3d9f51afb54ca89764
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-08 23:45:39 -0500

    doc: add documentation to BooleanHasher class

--- BooleanHasher.java
+++ BooleanHasher.java
@@ -1,6 +1,14 @@
 import java.security.*;
 
 public class BooleanHasher {
+     /**
+     * Hashes the given input string using the specified hashing algorithm.
+     *
+     * @param input the string to be hashed
+     * @param algorithm the hash algorithm to be used (MD5, SHA-1, SHA-256)
+     * @return the hashed string in hexadecimal format
+     * @throws IllegalArgumentException if the provided algorithm is unsupported
+     */
     public static String hashString(String input, HashAlgorithm algorithm) {
         try {
             MessageDigest digest = MessageDigest.getInstance(algorithm.name().replace('_', '-'));
@@ -14,6 +22,14 @@ public class BooleanHasher {
             throw new IllegalArgumentException("Invalid hash algorithm: " + algorithm, e);
         }
     }
+     /**
+     * Verifies whether a given string matches a given hash using the specified algorithm.
+     *
+     * @param input      The original string to verify.
+     * @param givenHash  The hash to compare against.
+     * @param algorithm  The hashing algorithm used to compute the hash.
+     * @return True if the hash of the input matches the given hash; false otherwise.
+     */
 
     public static boolean isHashMatch(String input, String givenHash, HashAlgorithm algorithm) {
         String computedHash = hashString(input, algorithm);
