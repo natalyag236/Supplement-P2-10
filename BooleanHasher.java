41167dd35b28d52cc914ee3ef91707235cf3f039
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-08 23:42:30 -0500

    feat: implement booleanhasher for hasing and hash matching

--- BooleanHasher.java
+++ BooleanHasher.java
@@ -1,14 +1,6 @@
 import java.security.*;
 
-public class StringHasher {
-    /**
-     * Hashes the given input string using the specified hashing algorithm.
-     *
-     * @param input the string to be hashed
-     * @param algorithm the hash algorithm to be used (MD5, SHA-1, SHA-256)
-     * @return the hashed string in hexadecimal format
-     * @throws IllegalArgumentException if the provided algorithm is unsupported
-     */
+public class BooleanHasher {
     public static String hashString(String input, HashAlgorithm algorithm) {
         try {
             MessageDigest digest = MessageDigest.getInstance(algorithm.name().replace('_', '-'));
@@ -21,6 +13,11 @@ public class StringHasher {
         } catch (NoSuchAlgorithmException e) {
             throw new IllegalArgumentException("Invalid hash algorithm: " + algorithm, e);
         }
+    }
+
+    public static boolean isHashMatch(String input, String givenHash, HashAlgorithm algorithm) {
+        String computedHash = hashString(input, algorithm);
+        return computedHash.equals(givenHash);
     }
 }
 
