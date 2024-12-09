e615b46a5574b90138509101d81533e575187f8a
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-08 22:50:34 -0500

    doc: add documentation to stringhasher

--- StringHasher.java
+++ StringHasher.java
@@ -2,6 +2,14 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class StringHasher {
+    /**
+     * Hashes the given input string using the specified hashing algorithm.
+     *
+     * @param input the string to be hashed
+     * @param algorithm the hash algorithm to be used (MD5, SHA-1, SHA-256)
+     * @return the hashed string in hexadecimal format
+     * @throws IllegalArgumentException if the provided algorithm is unsupported
+     */
     public static String hashString(String input, HashAlgorithm algorithm) {
         try {
             MessageDigest digest = MessageDigest.getInstance(algorithm.name().replace('_', '-'));
